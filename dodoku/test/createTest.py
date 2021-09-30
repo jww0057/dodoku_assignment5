dec6b5ce833f016af4982b83f5287227a11a322b
John Wood <jww0057@auburn.edu>
2021-09-29 19:18:26 -0500

    Red Light |

--- dodoku/test/createTest.py
+++ dodoku/test/createTest.py
@@ -2,4 +2,10 @@ from unittest import TestCase
 import dodoku.create as create 
 
 class CreateTest(TestCase):
-        pass
+    def test_create_010_ShouldReturnThreeParms(self):
+        level1 = {'op': 'create', 'level':'1'}
+        expectedResult = {"grid", "integrity", "status"}
+        actualResult = create._create(level1)
+        self.assertIn("grid", actualResult)
+        self.assertIn("integrity", actualResult)
+        self.assertIn("status", actualResult)
